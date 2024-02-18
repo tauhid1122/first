042268edaae3b4b309591acb8359e209b537f9f9
Md Tauhid <127687911+tauhid1122@users.noreply.github.com>
2024-02-18 13:05:24 +0530

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -4,4 +4,17 @@
 
 void main(){
     cout << "hello world";
+    int age;
+    cout<<"enter your age : ";
+    cin>>age;
+    if(age>=18)
+      {cout<<"you are man";}
+    elseif(age<=12)
+      {
+        cout<<"you are a baby";
+      }
+    else
+      {
+        cout<<"you are a boy";
+      }
 }
