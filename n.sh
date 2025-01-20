e4d1b118054efa971e03b4daee39a1f82b0a4be3
Md Tauhid <127687911+tauhid1122@users.noreply.github.com>
2025-01-20 14:53:05 +0000

    Update n.sh

--- n.sh
+++ n.sh
@@ -8,3 +8,15 @@ pkg install x11-repo
 pkg install termux-x11-nightly
 pkg install tur-repo
 pkg install pulseaudio
+
+pkg install xfce4
+
+#If you want to install the firefox browser:
+
+pkg install tur-repo
+pkg install firefox
+
+#If you want to install VS Code:
+
+pkg install tur-repo
+pkg install code-oss
